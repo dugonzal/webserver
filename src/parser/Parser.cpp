150e9726e76ebf372c66e5a33495901d4c2adfe9
dugonzal <ciclocontinuo2@gmail.com>
2024-03-10 14:15:18 +0000

    cambio de latop

--- src/parser/Parser.cpp
+++ src/parser/Parser.cpp
@@ -6,7 +6,7 @@
 /*   By: Dugonzal <dugonzal@student.42urduliz.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/03 13:36:19 by Dugonzal          #+#    #+#             */
-/*   Updated: 2024/03/08 03:11:32 by Dugonzal         ###   ########.fr       */
+/*   Updated: 2024/03/08 09:26:48 by Dugonzal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,13 +21,12 @@ Parser::~Parser(void) {
 }
 
 Parser::Parser(const string &filename): BaseParser(filename) {
-  
   dataServers = new vector<string>*[nServers];
- 
+
   for (unsigned int i = 0; i < nServers; i++)
     dataServers[i] = new vector<string>;
 
-  splitServers() ;
+  splitServers();
 }
 
 Parser::Parser(const Parser &copy): BaseParser(copy), \
@@ -42,27 +41,30 @@ Parser &Parser::operator=(const Parser &copy) {
 }
 
 unsigned int  Parser::getServer(int nServer, unsigned int j) {
-
-  cout  << "  "  << nServer << endl;
-  while (++j < data.size()) {
+  while (j < data.size()) {
+    dataServers[nServer]->push_back(data[j]);
+    // delete the first line of the server
+    data.erase(data.begin() + j);
     if (data[j].find("};") != string::npos)
       break;
-    dataServers[nServer]->push_back(data[j]);
-    data[j] = ""; 
+
+    cout << dataServers[nServer]->at(dataServers[nServer]->size() - 1) << endl;
+    j++;
   }
-  return (j);
+  return (j - 1);
 }
 
 void  Parser::splitServers(void) {
-
   unsigned int n = 0;
-  for (unsigned int i = 0; i < data.size(); i++) { 
+  for (unsigned int i = 0; i < data.size(); i++) {
     cout << endl;
     if (n == nServers)
       break;
     i = getServer(++n, i);
-  }
-  for (unsigned int j = 0; j < nServers; j++) 
+  }/*
+  for (unsigned int j = 0; j < nServers; j++) {
+    cout << "Server " << j << endl;
     for (unsigned int i = 0; i < dataServers[j]->size(); i++) 
     cout << dataServers[j]->at(i) << endl << endl;
+  }*/
 }

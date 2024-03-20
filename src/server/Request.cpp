97d3457dd8d2674b73061f7526e75e5a30ee412b
Jokin <jokinaizpuru28@gmail.com>
2024-03-20 12:37:15 +0100

    master, added safe string management code for Client Input Header

--- src/server/Request.cpp
+++ src/server/Request.cpp
@@ -19,6 +19,7 @@ Request::Request( int _serverFd ) {
 	timeout.tv_usec = 0;
 	serverFd = _serverFd;
 	bzero(clientMsg, sizeof(clientMsg));
+	std::cout << std::endl << "------------INPUT_DATA----------------" << std::endl;
 	if ((clientFd = accept(_serverFd, (sockaddr *)&clientAddr, &addrClientLen)) < 0)
 		perror("error: accept");
 
@@ -37,11 +38,13 @@ Request::Request( int _serverFd ) {
 		return ;
 	} else // Data received, process it
 		std::cout << "Bytes received: " << returnedBytes << std::endl;
-
 	inputIsGood = true;
 	inputMethod = getMethod();
-	inputRoute = getRoute();
-	checkHttpVersion();
+	if (inputIsGood == true) {
+		inputRoute = getRoute();
+		if (inputIsGood == true)
+			checkHttpVersion();
+	}
 	std::cout << std::endl << "--------------INPUT--------------" << std::endl;
 	std::cout << std::endl << clientMsg << std::endl;
 
@@ -73,15 +76,37 @@ Request::~Request( void ) {}
 std::string	Request::getRoute( void ) {
 	std::string str(clientMsg);
 	std::stringstream ss;
+	size_t start;
 
-	size_t start = str.find("/", 0);
-	if (str[start] == '/' && str[start + 1] == ' ')
-		return ("");
-	else {
-		for (start = start + 1; str[start] != ' '; start++) {
-			ss << str[start];
+
+	for (start = 0; str[start]; start++) {
+		if (start > 0 && str[start - 1] == ' ')
+			break;
+		else if (str[start] == '\n') {
+			inputIsGood = false;
+			return ("");
 		}
 	}
+	if (str[start] && str[start + 1]) {
+		if (str[start] == '/' && isspace(str[start + 1])) {
+			std::cout << "Route : " << "/" << std::endl;
+			return ("");
+		}
+		else if (str[start] == '/' && isalnum(str[start + 1])) {
+			for (start = start + 1; str[start] != ' '; start++) {
+				ss << str[start];
+			}
+		}
+		else {
+			inputIsGood = false;
+			return "";
+		}
+	}
+	else {
+		inputIsGood = false;
+		return "";
+	}
+	
 	std::cout << "Route : " << ss.str() << std::endl;
 	std::string ret(ss.str());
 	return (ret);
@@ -90,7 +115,7 @@ std::string	Request::getRoute( void ) {
 int		Request::getMethod( void ) {
 	std::stringstream ss;
 
-	for ( int i = 0; !isspace(clientMsg[i]); i++)
+	for ( int i = 0; !isspace(clientMsg[i]) && clientMsg[i]; i++)
 		ss << clientMsg[i];
 
 	std::cout << "Method : " << ss.str() << std::endl;
@@ -109,13 +134,26 @@ int		Request::getMethod( void ) {
 
 void	Request::checkHttpVersion( void ) {
 	std::string str(clientMsg);
+	std::string firstLine;
 	std::stringstream ss;
 
-	size_t pos;
-	for (pos = str.find("HTTP", 0); !isnumber(str[pos]); pos++)
-		;
-	while (!isspace(str[pos]))
-		ss << str[pos++];
+	firstLine = str.substr(0, str.find("\n", 0));
+	size_t pos = firstLine.find("HTTP/", 0);
+	if (pos == std::string::npos) {
+		inputIsGood = false;
+		perror("error: HTTP version is erroneus");
+		return ;
+	}
+	while (firstLine[pos] && !isnumber(firstLine[pos])) {
+		if (!firstLine[pos + 1] || isspace(firstLine[pos + 1])) {
+			inputIsGood = false;
+			perror("error: HTTP version: bad format");
+			return ;
+		}
+		pos++;
+	}
+	while (firstLine[pos] && !isspace(firstLine[pos]))
+		ss << firstLine[pos++];
 	
 	if (!ss.str().compare("1.1"))
 		std::cout << "Version : " << ss.str() << std::endl;
@@ -123,5 +161,6 @@ void	Request::checkHttpVersion( void ) {
 		inputIsGood = false;
 		perror("error: HTTP version is erroneus");
 	}
+	ss.clear();
 	std::cout << std::endl;
 }

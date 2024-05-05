9a63920e2034d3b59e34d25054cc7ae067acf5cc
Duvan Gonzalez <dugonzal@c3r2s3.42urduliz.com>
2024-05-05 14:37:44 +0200

    end end end prev sin ey

--- resources/ey/resources/main.py
+++
@@ -1,9 +0,0 @@
-print("hello world");
-print("hello world");
-print("hello world");
-print("hello world");
-print("hello world");
-print("hello world");
-print("hello world");
-print("modificado");
-print("hello world");

1187d1ece159977fdc154fec1824c8109a75749c
JOKITON <jokinaizpuru28@gmail.com>
2024-05-20 10:44:59 +0200

    Add more location tests & improve syntax

--- resources/cgi/main.py
+++ resources/cgi/main.py
@@ -8,3 +8,4 @@ print("hello world");
 print("modificado");
 #print(name);
 print("hello world");
+print("<form action=\"\" method=\"post\">\n\r\r\r<input type=\"submit\" name=\"upvote\" value=\"Upvote\" />")
